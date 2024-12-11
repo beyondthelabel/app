db2fc7c2e87293bb6204cab1299274c594a714c2
Brandon Moffett <ucrengineerpy@gmail.com>
2024-12-10 20:32:45 -0800

    added git ignore file

--- obj/Debug/net9.0/BeyondTheLabel.AssemblyInfo.cs
+++ obj/Debug/net9.0/BeyondTheLabel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeyondTheLabel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ade3c5ccf700b8f991ce5647a8c86eb2dd5deb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3a6d2dde387cf9ddac8e62cece6b9866d23c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeyondTheLabel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeyondTheLabel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
